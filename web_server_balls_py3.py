d9eabad1fdd954fc6392ff88b8e5aca2da072568
Valentyn Vovk <vivkvv@gmail.com>
2022-11-18 00:29:23 +0200

    1. renaming variables in python file; 2. functonality for max and fit buttin (did not finish)

--- web_server_balls_py3.py
+++ web_server_balls_py3.py
@@ -12,7 +12,7 @@ import os.path
 template_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.getcwd()))
 
-dispatcherIP = "20.203.227.129"
+DISPATCHER_IP = "20.203.227.129"
 
 app = Flask(__name__)
 
@@ -25,12 +25,12 @@ def before_all_requests():
 # Fix it!
 @app.route('/dispatcher', methods=['GET'])
 def set_ip_of_rooms_dispacher(vmIP):
-    vm_v = request.args.get('vmIP')
+    vm_ip = request.args.get('vmIP')
     pass
 
 @app.route('/viewGame', methods=['GET', 'POST'])
 def get_view_game():
-    vars = __get_variables('view.html')
+    view_var = __get_variables('view.html')
     template = template_env.get_template('view.html')
     data = {
         'roomMode': request.args['roomMode'],
@@ -38,46 +38,46 @@ def get_view_game():
         'roomPort': request.args['roomPort'],
         'webRTCGameId': request.args['webRTCGameId']
     }
-    outHtml = template.render(data)
-    return outHtml
+    out_html = template.render(data)
+    return out_html
 
 @app.route('/static/colormaps/colormaps.txt', methods=['GET', 'POST'])
 def get_possible_colormaps():
     template = template_env.get_template('/static/colormaps/colormaps.txt')
-    outHtml = template.render()
-    return outHtml
+    out_html = template.render()
+    return out_html
 
 @app.route('/static/colormaps/<path:path>', methods=['GET', 'POST'])
 def get_colormap(path):
     template = template_env.get_template('/static/colormaps/' + path)
-    outHtml = template.render()
-    return outHtml
+    out_html = template.render()
+    return out_html
 
 @app.route('/static/games/games.txt', methods=['GET', 'POST'])
 def get_possible_games():
     template = template_env.get_template('/static/games/games.txt')
-    outHtml = template.render()
-    return outHtml
+    out_html = template.render()
+    return out_html
 
 @app.route('/', methods=['GET', 'POST'])
 def get_main_page():
-    global dispatcherIP
-    vmIP = request.args.get('vmIP')
-    if(vmIP == "vm"):
-        dispatcherIP = request.remote_addr
-    elif(vmIP is None):
-        vars = __get_variables('main.html')
+    global DISPATCHER_IP
+    vm_ip = request.args.get('vmIP')
+    if(vm_ip == "vm"):
+        DISPATCHER_IP = request.remote_addr
+    elif(vm_ip is None):
+        main_html_var = __get_variables('main.html')
         template = template_env.get_template('main.html')
-        data = {list(vars)[0]: dispatcherIP}
-        outHtml = template.render(data)
-        return outHtml
+        data = {list(main_html_var)[0]: DISPATCHER_IP}
+        out_html = template.render(data)
+        return out_html
 
 def __get_variables(filename):
     template_src = template_env.loader.get_source(template_env, filename)
     template_source = template_src[0]
     parsed_content = template_env.parse(template_source)
-    mt = jinja2.meta
-    variables = mt.find_undeclared_variables(parsed_content)
+    jinja_meta = jinja2.meta
+    variables = jinja_meta.find_undeclared_variables(parsed_content)
     return variables
 
 
